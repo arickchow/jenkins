042856cc9860b9da1b932f7a310bdcbe8ca2b104
Arick Chow <arickchow@gmail.com>
2013-07-09 15:19:06 -0700

    new test

--- square_test.py
+++ square_test.py
@@ -1,10 +1,15 @@
 from square import square
 
 result = [0,1,4,9,16,25,36,49,64,81,100]
+not_square = [0,100,81,64,49,35,25,16,9,4,1]
 
 def test_square():
 	for value in range(1,11):
 		assert square(value) == result[value]
+
+def test_not_square():
+	for value in range(1,11):
+		assert not square(value) == not_square[value]
 
 
 
