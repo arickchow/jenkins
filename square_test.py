cbe10d52d9c153c0b90b740041117c37cc9c2691
Arick Chow <arickchow@gmail.com>
2013-07-09 16:59:32 -0700

    changed to nose asserts

--- square_test.py
+++ square_test.py
@@ -6,14 +6,13 @@ not_square = [0,100,81,64,49,35,25,16,9,4,1]
 
 def test_square():
 	for value in range(1,11):
-		assert square(value) == result[value]
-
+		nose.tools.assert_equal(square(value),result[value])
 def test_not_square():
 	for value in range(1,11):
-		assert not square(value) == not_square[value]
+		nose.tools.assert_not_equal(square(value),not_square[value])
 	
 	for value in range(1,11):
-		assert not square(value+1) == result[value]
+		nose.tools.assert_not_equal(square(value+1),result[value])
 	
 	for value in range(1,11):
-		assert not square(value+2) == result[value]
+		nose.tools.assert_not_equal(square(value+2),result[value])
