15b80a6b8b29bf316ed95dece9a18a7a960331a9
Arick Chow <arickchow@gmail.com>
2013-07-09 11:48:19 -0700

    test

--- square_test.py
+++ square_test.py
@@ -1,6 +1,12 @@
 from square import square
-result = [1,4,9,16,25,36,49,64,81,100]
 
+def setup_func():
+	result = [1,4,9,16,25,36,49,64,81,100]
+
+def teardown_func():
+	pass
+
+@with_setup(setup_func,teardown_func)
 def test_square():
 	for value in range(1,11):
 		assert square(value) == result[value]
