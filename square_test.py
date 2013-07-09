f448f937373f944496b359fc2413fcb5eb393b82
Arick Chow <arickchow@gmail.com>
2013-07-09 11:19:58 -0700

    fix

--- square_test.py
+++ square_test.py
@@ -1,9 +1,5 @@
-def setup():
-	result = [1,4,9,16,25,36,49,64,81,100]
-def teardown():
-	pass
+result = [1,4,9,16,25,36,49,64,81,100]
 
-@with_setup(setup,teardown)
 def test_square():
 	for value in range(1,11):
 		assert square(value) == result[value]
